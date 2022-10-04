dc522d0ea5737c1ebe2f892de55790dd4c1d8f1a
oscar <oscar@localhost>
2022-10-04 22:04:27 +0200

    Version modificada de una calculadora simple para pruebas unitarias

---
+++ src/pruebaunitaria/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebaunitaria;
+
+/**
+ *
+ * @author oscar
+ */
+public class Calculadora {
+    public int sumarNumeros(int a, int b){
+            return a+b;
+    }
+    
+}

2066428b489fa821a649f5033da68584847af186
oscar <oscar@localhost>
2022-10-04 21:57:54 +0200

    Version inicial de una calculadora simple para pruebas unitarias

---
+++ src/pruebaunitaria/PruebaUnitaria.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaunitaria;
+
+/**
+ *
+ * @author oscar
+ */
+public class PruebaUnitaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Prueba manual tradicional
+        Calculadora objeto = new Calculadora();
+        int resultado = objeto.sumarNumeros(3,3);
+        
+        System.out.println(resultado);                      
+    }
+    
+}
